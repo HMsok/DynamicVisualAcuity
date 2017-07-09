6c45eb882a44119b8d333ec4ba01af08ccadc517
Hiroki <Hiroki@USER-no-MacBook-Pro.local>
2017-07-10 01:58:27 +0900

    refs #3 add count down timer

--- app/src/main/java/com/example/hiroki/dynamicvisualacuitytest/Main2Activity.java
+++ app/src/main/java/com/example/hiroki/dynamicvisualacuitytest/Main2Activity.java
@@ -1,7 +1,11 @@
 package com.example.hiroki.dynamicvisualacuitytest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,11 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -22,21 +24,24 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+public class Main2Activity extends AppCompatActivity {
 
-    private List<Button> $buttons = new ArrayList<>();
+    // This will display when max number is clicked
+    private Button $nextGameButton;
+    private TableLayout $tableLayout;
+    private TextView $timerText;
+    private TextView $countDownTimerText;
+    private TextView $nextNumberDisplay;
+    private List<Button> buttons = new ArrayList<>();
+    private int lastPushedNumber = 0;
+    private Map<Button, DynamicVisualAcuityDto> dtoMap = new HashMap<>();
+    private Timer timer;
+    private CountDownTimer countDownTimer;
+    private int count = 0;
 
     private static final int MAX_NUMBER = 25;
-
-    private Map<Button, DynamicVisualAcuityDto> dtoMap = new HashMap<>();
-
-    private int lastPushedNumber = 0;
-
-    private TextView $timerText;
-
-    private Timer timer;
-
-    private int count = 0;
+    private static final long START_COUNT = 4000; // msec
+    private static final long INTERVAL_COUNT = 1000; // msec
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,56 +50,121 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.decorateInternal();
+        this.setInitializeView();
+        this.bindEvents();
+        countDownTimer.start();
+    }
+
+    private void decorateInternal() {
+        $tableLayout = (TableLayout) findViewById(R.id.tableRayout);
+        $timerText = (TextView) findViewById(R.id.timerText);
+        $nextNumberDisplay = (TextView) findViewById(R.id.nextNumber);
+        $nextGameButton = (Button) findViewById(R.id.nextGame);
+        $countDownTimerText = (TextView) findViewById(R.id.countDownTimerText);
+        buttons = this.makeButtonsList(buttons);
+        Collections.shuffle(buttons);
+        dtoMap = this.createDtoMap(buttons, MAX_NUMBER);
+        countDownTimer = new CountDownTimer(this, START_COUNT, INTERVAL_COUNT);
+    }
+
+    private void setInitializeView() {
+        $timerText.setText("00:00.0");
+        $countDownTimerText.setText(countDownTimer.START_COUNT);
+
+        for(Map.Entry<Button, DynamicVisualAcuityDto> entry : dtoMap.entrySet()) {
+            Button $btn = entry.getKey();
+            DynamicVisualAcuityDto dto = entry.getValue();
+            $btn.setText(Integer.toString(dto.no));
+        }
+
+        $nextNumberDisplay.setText(Integer.toString(lastPushedNumber + 1));
+    }
+
+    private void bindEvents() {
+        // set nextGameButton
+        $nextGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onClickNextGameBtn();
             }
         });
 
-        $buttons = this.makeButtonsList($buttons);
-        Collections.shuffle($buttons);
-
-        dtoMap = this.createDtoMap($buttons, MAX_NUMBER);
-        this.setButtonTextAndEvent(dtoMap);
-        $timerText = (TextView) findViewById(R.id.timerText);
-        this.initTimerText();
+        for (Map.Entry<Button, DynamicVisualAcuityDto> entry : dtoMap.entrySet()) {
+            Button $btn = entry.getKey();
+            DynamicVisualAcuityDto dto = entry.getValue();
+            if (dto.no == this.MAX_NUMBER) {
+                $btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finishGame();
+                    }
+                });
+            } else {
+                $btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onClickButton(view);
+                    }
+                });
+            }
+        }
     }
 
-    private void initTimerText() {
-        $timerText.setText("00:00.0");
+    private void onClickButton(View v) {
+        DynamicVisualAcuityDto dto = dtoMap.get(v);
+        if(dto.no == lastPushedNumber + 1) {
+            dto.isPushed = true;
+            lastPushedNumber++;
+            $nextNumberDisplay.setText(Integer.toString(lastPushedNumber + 1));
+            $nextNumberDisplay.setTextColor(Color.BLACK);
+        } else {
+            $nextNumberDisplay.setText(Integer.toString(lastPushedNumber + 1) + "!");
+            $nextNumberDisplay.setTextColor(Color.RED);
+        }
     }
 
-    private List<Button> makeButtonsList(List<Button> $buttons) {
-        $buttons.add((Button) findViewById(R.id.button00));
-        $buttons.add((Button) findViewById(R.id.button01));
-        $buttons.add((Button) findViewById(R.id.button02));
-        $buttons.add((Button) findViewById(R.id.button03));
-        $buttons.add((Button) findViewById(R.id.button04));
-        $buttons.add((Button) findViewById(R.id.button10));
-        $buttons.add((Button) findViewById(R.id.button11));
-        $buttons.add((Button) findViewById(R.id.button12));
-        $buttons.add((Button) findViewById(R.id.button13));
-        $buttons.add((Button) findViewById(R.id.button14));
-        $buttons.add((Button) findViewById(R.id.button20));
-        $buttons.add((Button) findViewById(R.id.button21));
-        $buttons.add((Button) findViewById(R.id.button22));
-        $buttons.add((Button) findViewById(R.id.button23));
-        $buttons.add((Button) findViewById(R.id.button24));
-        $buttons.add((Button) findViewById(R.id.button30));
-        $buttons.add((Button) findViewById(R.id.button31));
-        $buttons.add((Button) findViewById(R.id.button32));
-        $buttons.add((Button) findViewById(R.id.button33));
-        $buttons.add((Button) findViewById(R.id.button34));
-        $buttons.add((Button) findViewById(R.id.button40));
-        $buttons.add((Button) findViewById(R.id.button41));
-        $buttons.add((Button) findViewById(R.id.button42));
-        $buttons.add((Button) findViewById(R.id.button43));
-        $buttons.add((Button) findViewById(R.id.button44));
+    private void finishGame() {
+        $nextGameButton.setVisibility(View.VISIBLE);
+        $nextGameButton.setText("ホームに戻る");
+        Toast.makeText(getApplicationContext(), "Game Clear!", Toast.LENGTH_LONG).show();
+        timer.cancel();
+    }
 
-        return $buttons;
+    public void onClickNextGameBtn() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//        resetTimer();
+        startActivity(i);
+    }
+
+
+    private List<Button> makeButtonsList(List<Button> buttons) {
+        buttons.add((Button) findViewById(R.id.button00));
+        buttons.add((Button) findViewById(R.id.button01));
+        buttons.add((Button) findViewById(R.id.button02));
+        buttons.add((Button) findViewById(R.id.button03));
+        buttons.add((Button) findViewById(R.id.button04));
+        buttons.add((Button) findViewById(R.id.button10));
+        buttons.add((Button) findViewById(R.id.button11));
+        buttons.add((Button) findViewById(R.id.button12));
+        buttons.add((Button) findViewById(R.id.button13));
+        buttons.add((Button) findViewById(R.id.button14));
+        buttons.add((Button) findViewById(R.id.button20));
+        buttons.add((Button) findViewById(R.id.button21));
+        buttons.add((Button) findViewById(R.id.button22));
+        buttons.add((Button) findViewById(R.id.button23));
+        buttons.add((Button) findViewById(R.id.button24));
+        buttons.add((Button) findViewById(R.id.button30));
+        buttons.add((Button) findViewById(R.id.button31));
+        buttons.add((Button) findViewById(R.id.button32));
+        buttons.add((Button) findViewById(R.id.button33));
+        buttons.add((Button) findViewById(R.id.button34));
+        buttons.add((Button) findViewById(R.id.button40));
+        buttons.add((Button) findViewById(R.id.button41));
+        buttons.add((Button) findViewById(R.id.button42));
+        buttons.add((Button) findViewById(R.id.button43));
+        buttons.add((Button) findViewById(R.id.button44));
+        return buttons;
     }
 
     public Map<Button ,DynamicVisualAcuityDto> createDtoMap(List<Button> $shuffledButtons, int maxNumber) {
@@ -108,52 +178,28 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
             DynamicVisualAcuityDto dto = new DynamicVisualAcuityDto();
             dto.no = i;
             dto.isPushed = false;
-            dtoMap.put($buttons.get(i - 1), dto);
+            dtoMap.put(buttons.get(i - 1), dto);
         }
         return dtoMap;
     }
 
-    private void setButtonTextAndEvent(Map<Button, DynamicVisualAcuityDto> dtoMap) {
-        for (Map.Entry<Button, DynamicVisualAcuityDto> entry : dtoMap.entrySet()) {
-            Button $btn = entry.getKey();
-            $btn.setText(Integer.toString(entry.getValue().no));
-            $btn.setOnClickListener(this);
-        }
+    public void startGame() {
+        $tableLayout.setVisibility(View.VISIBLE);
+        $countDownTimerText.setVisibility(View.INVISIBLE);
+
+//        this.resetTimer();
+        this.startTimer();
     }
 
-    @Override
-    public void onClick(View v){
-        DynamicVisualAcuityDto dto = dtoMap.get(v);
-        if(dto.no == lastPushedNumber + 1) {
-            dto.isPushed = true;
-            lastPushedNumber++;
+//    private void resetTimer() {
+//        if (null != timer) {
+//            timer.cancel();
+//            timer = null;
+//            count = 0;
+//        }
+//    }
 
-            if(dto.no == 1) {
-                this.startGame();
-            }
-
-            // finished game
-            if(dto.no == MAX_NUMBER) {
-                this.finishGame();
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "間違い", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void finishGame() {
-        Toast.makeText(getApplicationContext(), "Game Clear!", Toast.LENGTH_LONG).show();
-        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-
-//        resetTimer();
-        stopTimer();
-//        startActivity(i);
-    }
-
-
-    private void startGame() {
-        this.resetTimer();
-
+    private void startTimer() {
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -167,16 +213,25 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         },0 , 100);
     }
 
-    private void resetTimer() {
-        if (null != timer) {
-            timer.cancel();
-            timer = null;
-            this.initTimerText();
-            count = 0;
-        }
-    }
+    class CountDownTimer extends android.os.CountDownTimer {
 
-    private void stopTimer() {
-        timer.cancel();
+        private Main2Activity outerClass;
+
+        final String START_COUNT = Integer.toString(3);
+
+        CountDownTimer(Main2Activity main2Activity, long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            this.outerClass = main2Activity;
+        }
+        @Override
+        public void onTick(long l) {
+            String countDownTimerText = Long.toString(l / 1000 % 60);
+            outerClass.$countDownTimerText.setText(countDownTimerText);
+        }
+
+        @Override
+        public void onFinish() {
+            outerClass.startGame();
+        }
     }
 }
